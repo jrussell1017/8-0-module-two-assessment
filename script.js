39f4e3618829afb7b7b415063d0ca84b926c9f29
james russell <jamesrussell@pursuit.org>
2021-10-14 14:54:32 -0400

    got dropdown to create display when chosen

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
-
-
+let movieSelector = document.querySelector("#movie-dropdown")
+let display = document.querySelector("#display-info")
 
 fetch(`https://ghibliapi.herokuapp.com/films`)
 .then((res) => {
     return res.json()
-    .then((data) => {
+    .then((movies) => {
 
-        let movieName = data.title
-        let releaseDate = data.release_date
-        let movieDescription = data.description
-
-        let movieSelector = document.querySelector("#movie-dropdown")
-            console.log(data)
         
-        for(let movie of data) {
+        console.log(movies)
+        
+        for(let movie of movies) {
             let option = document.createElement("option")
             option.textContent = movie.title
             option.value = movie.title
             movieSelector.append(option)
         }
-
-        movieSelector.addEventListener("", () => {
-            
-        })
-
-
-    
         
+        
+        movieSelector.addEventListener("change", (e) => {
+           e.preventDefault()
+
+           
+           for(let movie of movies) {
+                let movieName = movie.title
+                let releaseYear = movie.release_date
+                let movieDescription = movie.description
+
+                if(e.target.value === movieName) {
+                display.innerHTML = 
+                    `<h3 id="movie-name">${movieName}</h2>
+                    <p id="release-year">${releaseYear}</p>
+                    <p id="movie-description">${movieDescription}</p>`   
+                }                 
+            }
+            
+
+            // let h3 = document.createElement("h3")
+            // h3.textContent = movieName
+            // display.append(h3)
+
+            // let releaseYear = document.createElement("p")
+            // releaseYear.textContent = releaseDate
+            // display.append(releaseYear)
+
+            // let description = document.querySelector("p")
+            // description.textContent = movieDescription
+            // display.append(description)
+
+        })
+    })
+        .catch((err) => {
+            console.log(err)
         })
     })
