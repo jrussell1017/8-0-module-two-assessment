0b60f16b94d6a3c3170f24b31dab9ed37007478b
james russell <jamesrussell@pursuit.org>
2021-10-14 17:53:47 -0400

    tried to get layout correct

--- script.js
+++ script.js
@@ -1,53 +1,48 @@
-let movieSelector = document.querySelector("#movie-dropdown")
-let display = document.querySelector("#display-info")
+let movieSelector = document.querySelector("#movie-dropdown");
+let display = document.querySelector("#display-info");
 
-fetch(`https://ghibliapi.herokuapp.com/films`)
-.then((res) => {
-    return res.json()
+fetch(`https://ghibliapi.herokuapp.com/films`).then((res) => {
+  return res
+    .json()
     .then((movies) => {
+      console.log(movies);
 
-        
-        console.log(movies)
-        
-        for(let movie of movies) {
-            let option = document.createElement("option")
-            option.textContent = movie.title
-            option.value = movie.title
-            movieSelector.append(option)
-        }
-        
-        
-        movieSelector.addEventListener("change", (e) => {
-           e.preventDefault()
+      for (let movie of movies) {
+        let option = document.createElement("option");
+        option.textContent = movie.title;
+        option.value = movie.title;
+        movieSelector.append(option);
+      }
 
-           for(let movie of movies) {
-                let movieName = movie.title
-                let releaseYear = movie.release_date
-                let movieDescription = movie.description
+      movieSelector.addEventListener("change", (e) => {
+        e.preventDefault();
 
-                if(e.target.value === movieName) {
-                display.innerHTML = 
-                    `<h3 id="movie-name">${movieName}</h2>
+        for (let movie of movies) {
+          let movieName = movie.title;
+          let releaseYear = movie.release_date;
+          let movieDescription = movie.description;
+
+          if (e.target.value === movieName) {
+            display.innerHTML = `<h3 id="movie-name">${movieName}</h2>
                     <p id="release-year">${releaseYear}</p>
-                    <p id="movie-description">${movieDescription}</p>`   
-                }                 
-            }
+                    <p id="movie-description">${movieDescription}</p>`;
+          }
+        }
 
-            let form = document.querySelector("form")
-            form.addEventListener("submit", (e) => {
-                e.preventDefault()
-                
-                let ul = document.querySelector("ul")
-                let li = document.createElement("li")
-                
-                ul.append(li)
-                li.innerHTML = `<strong>${movieSelector.value}:</strong> ${e.target['text-input'].value}`
-                e.target.reset()
-            })
+        let form = document.querySelector("form");
+        form.addEventListener("submit", (e) => {
+          e.preventDefault();
 
-        })
+          let ul = document.querySelector("ul");
+          let li = document.createElement("li");
+
+          li.innerHTML = `<strong>${movieSelector.value}:</strong> ${e.target["text-input"].value}`;
+          ul.append(li);
+          e.target.reset();
+        });
+      });
     })
-        .catch((err) => {
-            console.log(err)
-        })
-    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
